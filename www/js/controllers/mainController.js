4a50e85a733909e46dde2857863506f223509e13
victtavares <victtavares1@gmail.com>
2014-12-10 15:22:58 -0600

    Basic Features done.
    No bluetooth connection

--- www/js/controllers/mainController.js
+++ www/js/controllers/mainController.js
@@ -1,38 +1,30 @@
-var safebandCtrl = angular.module('controllers.main',[]);
+var safebandCtrl = angular.module('controllers.main',['services.mail','service.localStorage']);
 
-safebandCtrl.controller('mainCtrl',function($rootScope, $scope, $firebase, $http) {
+safebandCtrl.controller('mainCtrl',function($rootScope, $scope, $firebase, $http,serviceMail,localStorage,$ionicLoading,$ionicPopup) {
     ref = new Firebase($rootScope.firebaseRef + "/contacts");
     var sync = $firebase(ref);
     var contacts = sync.$asArray();
-    //$rootScope.checkSession();
+    $scope.data  = {};
+    $scope.data.lastAlert = localStorage.get("lastAlert","noAlert");
+
+
+
 
     $scope.sendAlert = function() {
-        var to = "";
-        var message = "";
-        var subject = "Possible friend in Danger!";
-        //console.log(contacts);
-        for(i = 0; i < contacts.length; i++) {
-            if (i == contacts.length -1) {
-                to += contacts[i].contact_email;
-            } else {
-                to += contacts[i].contact_email + ",";
-                message = "Your friend with e-mail " + contacts[i].user_email + " may be in dangerous, please contact him!";
-            }
-        }
-        console.log(to);
-        console.log(subject);
-        console.log(message);
-        $http.post('http://sendemail-safeband.rhcloud.com/sendEmail.php',
-            {to: to, subject: subject, message:message}, {timeout: 3000})
-            .success(function(data) {
-                console.log(data);
-                console.log("foi");
+        $scope.data.lastAlert = localStorage.get("lastAlert","noAlert");
+        serviceMail.sendMail(contacts)
+            .success( function(data) {
+                $ionicPopup.alert({
+                            title: 'Alert e-mail was sent to all your contacts!'
+                    });
             })
-            .error(function (data) {
-                console.log(data);
-
-                console.log(" não foi");
+            .error(function() {
+                $ionicPopup.alert({
+                    title: 'Error- Try again Later!'
+                });
             });
 
-            }
+        $ionicLoading.hide();
+
+    }
 });
---
+++ www/js/services/mailService.js
@@ -0,0 +1,35 @@
+var earthService = angular.module('services.mail', ['service.localStorage']);
+
+
+earthService.factory('serviceMail', function($http, localStorage) {
+        var service = {};
+
+
+        service.sendMail = function (contacts) {
+            var to = "";
+            var message = "";
+            var subject = "Possible friend in Danger!";
+
+            if (contacts[0]) {
+                message = "Your friend with e-mail " + contacts[0].user_email + " may be in dangerous, please contact him!";
+            }
+
+            for(i = 0; i < contacts.length; i++) {
+                if (i == contacts.length -1) {
+                    to += contacts[i].contact_email;
+                } else {
+                    to += contacts[i].contact_email + ",";
+
+                }
+            }
+            var d = new Date();
+            var time = d.toLocaleString();
+            localStorage.set("lastAlert",time);
+
+             return $http.post('http://sendemail-safeband.rhcloud.com/sendEmail.php',
+                {to: to, subject: subject, message:message}, {timeout: 3000});
+
+        }
+
+        return service;
+    });
